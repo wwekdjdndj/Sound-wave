c66a90149a51fa23c076d58ea3e1f28fe7cd3562
wwekdjdndj <101439099+wwekdjdndj@users.noreply.github.com>
2022-05-30 13:21:47 -0400

    Add files via upload

---
+++ commands/music/autoplay.js
@@ -0,0 +1,86 @@
+const config = require('../../botconfig/config.json');
+const {
+    MessageEmbed
+} = require('discord.js');
+
+module.exports = {
+    name: 'autoplay',
+    aliases: [],
+    usage: '',
+    description: '',
+    cooldown: 0,
+    userPermissions: [],
+    botPermissions: [],
+    // toggleOff: true,
+
+    run: async (client, message, args, ee) => {
+        try {
+
+            const {
+                member,
+                guild,
+            } = message;
+
+            const {
+                channel
+            } = member.voice;
+
+
+            const VoiceChannel = member.voice.channel;
+
+            if (!VoiceChannel) return message.reply({
+                embeds: [new MessageEmbed()
+                    .setColor(ee.wrongcolor)
+                    .setTimestamp()
+                    .setTitle(`${client.allEmojis.x} Please Join a Voice Channel`)
+                ]
+            });
+
+            if (channel.userLimit != 0 && channel.full)
+                return message.reply({
+                    embeds: [new MessageEmbed()
+                        .setColor(ee.wrongcolor)
+                        .setFooter(ee.footertext, ee.footericon)
+                        .setTitle(`Your Voice Channel is full, I can't join!`)
+                    ]
+                });
+
+
+            if (guild.me.voice.channelId && VoiceChannel.id !== guild.me.voice.channelId) return message.reply({
+                embeds: [new MessageEmbed()
+                    .setColor(ee.wrongcolor)
+                    .setTimestamp()
+                    .setTitle(`I am already playing music in <#${guild.me.voice.channelId}>`)
+                ]
+            });
+
+            const queue = await client.distube.getQueue(VoiceChannel);
+            if (!queue) return message.reply({
+                embeds: [new MessageEmbed()
+                    .setColor(ee.wrongcolor)
+                    .setTimestamp()
+                    .setTitle(`${client.allEmojis.x} There is no Song in the Queue.`)
+                ]
+            });
+
+            let Mode = await queue.toggleAutoplay(VoiceChannel);
+            return message.reply({
+                embeds: [new MessageEmbed()
+                    .setColor(ee.color)
+                    .setTimestamp()
+                    .setTitle(`${client.allEmojis.music.autoplay} Autoplay Mode is set to: \`${Mode ? "On" : "Off"}\``)
+                ]
+            });
+
+
+        } catch (e) {
+            console.log(e)
+            return message.reply({
+                embeds: [new MessageEmbed()
+                    .setTitle(`⛔ Error`)
+                    .setDescription(`${e}`)
+                ]
+            })
+        }
+    }
+}
---
+++ commands/music/loop.js
@@ -0,0 +1,86 @@
+const config = require('../../botconfig/config.json');
+const {
+    MessageEmbed
+} = require('discord.js');
+
+module.exports = {
+    name: 'loop',
+    aliases: [],
+    usage: '',
+    description: '',
+    cooldown: 0,
+    userPermissions: [],
+    botPermissions: [],
+    // toggleOff: true,
+
+    run: async (client, message, args, ee) => {
+        try {
+
+            const {
+                member,
+                guild,
+            } = message;
+
+            const {
+                channel
+            } = member.voice;
+
+
+            const VoiceChannel = member.voice.channel;
+
+            if (!VoiceChannel) return message.reply({
+                embeds: [new MessageEmbed()
+                    .setColor(ee.wrongcolor)
+                    .setTimestamp()
+                    .setTitle(`${client.allEmojis.x} Please Join a Voice Channel`)
+                ]
+            });
+
+            if (channel.userLimit != 0 && channel.full)
+                return message.reply({
+                    embeds: [new MessageEmbed()
+                        .setColor(ee.wrongcolor)
+                        .setFooter(ee.footertext, ee.footericon)
+                        .setTitle(`Your Voice Channel is full, I can't join!`)
+                    ]
+                });
+
+
+            if (guild.me.voice.channelId && VoiceChannel.id !== guild.me.voice.channelId) return message.reply({
+                embeds: [new MessageEmbed()
+                    .setColor(ee.wrongcolor)
+                    .setTimestamp()
+                    .setTitle(`I am already playing music in <#${guild.me.voice.channelId}>`)
+                ]
+            });
+
+            const queue = await client.distube.getQueue(VoiceChannel);
+            if (!queue) return message.reply({
+                embeds: [new MessageEmbed()
+                    .setColor(ee.wrongcolor)
+                    .setTimestamp()
+                    .setTitle(`${client.allEmojis.x} There is no Song in the Queue.`)
+                ]
+            });
+
+            let Mode2 = await client.distube.setRepeatMode(queue);
+            return message.reply({
+                embeds: [new MessageEmbed()
+                    .setColor(ee.color)
+                    .setTimestamp()
+                    .setTitle(`${client.allEmojis.music.loop} Loop Mode is set to: \`${Mode2 = Mode2 ? Mode2 == 2 ? "Queue" : "Song" : "Off"}\``)
+                ]
+            });
+
+
+        } catch (e) {
+            console.log(e)
+            return message.reply({
+                embeds: [new MessageEmbed()
+                    .setTitle(`⛔ Error`)
+                    .setDescription(`${e}`)
+                ]
+            })
+        }
+    }
+}
